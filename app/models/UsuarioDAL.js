a8077271567c95ab985022a2b5fdbea0699f2076
bielzin21 <rm83826@estudante.fieb.edu.br>
2023-10-18 12:44:14 -0300

    editarPerfil

--- app/models/UsuarioDAL.js
+++ app/models/UsuarioDAL.js
@@ -58,13 +58,14 @@ module.exports = class UsuarioDAL {
     update(camposJson) {
         return new Promise((resolve, reject) => {
             this.accex.query(
-                "UPDATE usuario SET nome_usuario = ?, user_usuario = ?, senha_usuario = ?,  " +
-                " email_usuario = ?, fone_usuario = ?, tipo_usuario = ?, status_usuario = ? " +
-                " WHERE id_usuario = ?",
-                [camposJson.nome_usuario, camposJson.user_usuario, camposJson.senha_usuario,
-                camposJson.email_usuario, camposJson.fone_usuario, camposJson.tipo_usuario,
-                camposJson.status_usuario, camposJson.id_usuario
+                "UPDATE usuario SET nome_completo = ?, nickname = ?, senha = ?,  " +
+                " email = ?, telefone = ?, img_perfil = ?,id_tipo_usuario = ?, status_usuario = ? " +
+                " WHERE idusuario = ?",
+                [camposJson.nome_completo, camposJson.nickname, camposJson.senha,
+                camposJson.email, camposJson.telefone, camposJson.img_perfil,camposJson.id_tipo_usuario,
+                camposJson.status_usuario, camposJson.idusuario
                 ],
+                
                 function (error, results, fields) {
                     if (error) {
                         return reject(error);

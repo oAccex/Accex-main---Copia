75ab7db12c17c30d3c111701dd985b71c1d32631
bielzin21 <148264165+bielzin21@users.noreply.github.com>
2023-11-08 07:51:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// Use PORT provided in environment or default to 3000
-const port = process.env.PORT || 3000;
-
-// Listen on `port` and 0.0.0.0
-app.listen(port, "0.0.0.0", function () {
-  // ...
-});
-
 const express = require("express");
 const app = express();
 const port = 6969;
